3eb13efc38e0a6dee3542dffe96c00b971f089ed
bataemperor <bataemperor@yahoo.com>
2015-06-20 22:44:35 +0200

    IP address change fixed

--- app/src/main/java/com/example/klijentandr2/LoginActivity.java
+++ app/src/main/java/com/example/klijentandr2/LoginActivity.java
@@ -64,7 +64,8 @@ public class LoginActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
-
+            Intent intent = new Intent(this,SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
         return super.onOptionsItemSelected(item);
