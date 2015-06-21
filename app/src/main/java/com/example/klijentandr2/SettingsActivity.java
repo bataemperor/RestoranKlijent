272f0587b44bd6525ce33357ab856ec401fd6668
bataemperor <bataemperor@yahoo.com>
2015-06-21 13:03:39 +0200

    code refactoring

--- app/src/main/java/com/example/klijentandr2/SettingsActivity.java
+++ app/src/main/java/com/example/klijentandr2/SettingsActivity.java
@@ -2,13 +2,9 @@ package com.example.klijentandr2;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import komunikacija.Komunikacija;
 import utility.Utility;
@@ -25,7 +21,7 @@ public class SettingsActivity extends Activity {
 
     }
     public void postaviIP(View view){
-        if (etIPAdress.getText().toString()!= null && !"".equalsIgnoreCase(etIPAdress.getText().toString()) ){
+        if (etIPAdress.getText()!= null && !"".equalsIgnoreCase(etIPAdress.getText().toString()) ){
             Komunikacija.ipAddress= etIPAdress.getText().toString();
             Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
