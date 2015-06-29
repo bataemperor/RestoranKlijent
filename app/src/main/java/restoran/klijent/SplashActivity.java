a1d60c3f91c5b0d124d6e64849af1cb4f303e998
bataemperor <borgir111>
2015-06-29 11:54:40 +0200

    bug fixed

--- app/src/main/java/restoran/klijent/SplashActivity.java
+++ app/src/main/java/restoran/klijent/SplashActivity.java
@@ -5,14 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
-import android.view.animation.DecelerateInterpolator;
-import android.widget.ProgressBar;
-
-
 import com.example.activity.R;
-
 import fr.castorflex.android.smoothprogressbar.SmoothProgressBar;
-import fr.castorflex.android.smoothprogressbar.SmoothProgressDrawable;
 
 public class SplashActivity extends Activity {
     private static final int SPLASH_DISPLAY_LENGTH = 3000;
@@ -23,8 +17,9 @@ public class SplashActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         spb = (SmoothProgressBar) findViewById(R.id.smooth_progress);
-
+        spb.progressiveStart();
         spb.setVisibility(View.VISIBLE);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -32,9 +27,26 @@ public class SplashActivity extends Activity {
                 SplashActivity.this.startActivity(mainIntent);
 
                 SplashActivity.this.finish();
-                spb.setVisibility(View.INVISIBLE);
+
             }
         }, SPLASH_DISPLAY_LENGTH);
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        spb.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+    }
 }
